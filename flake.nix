736eb8b457727d57b6b45c53c2da9c66bbbe0c8e
polygon <polygon@wh2.tu-dresden.de>
2024-02-20 01:45:03 +0100

    Add proxy builder library function

--- flake.nix
+++ flake.nix
@@ -9,8 +9,12 @@
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    packages.${system} = {
-      test-buildproxy = pkgs.callPackage ./nix-buildproxy/buildproxy.nix { inherit self; proxy_content = (import ./example/proxy_content.nix); };
+    lib.${system}.proxy-builder = proxy-content-file: (pkgs.callPackage ./nix-buildproxy/buildproxy.nix { inherit self; proxy_content = (import proxy-content-file); });
+    packages.${system} = let
+      proxy-builder = self.lib.${system}.proxy-builder;
+    in
+    {
+      test-buildproxy = proxy-builder ./example/proxy_content.nix;
       test = pkgs.callPackage ./example/test.nix { inherit self; buildproxy = self.packages.${system}.test-buildproxy; };
       proxy-capture = pkgs.callPackage ./nix-buildproxy/proxy-capture.nix { inherit self; };
       buildproxy = pkgs.callPackage ./nix-buildproxy/buildproxy.nix { inherit self; };
